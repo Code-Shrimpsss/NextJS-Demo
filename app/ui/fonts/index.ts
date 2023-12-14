6ad9d3dfcb4d9f0ecd5d7d8ae48b8463e9df28b0
Vito Wang <w9783094562@gmail.com>
2023-12-15 11:32:12 +1300

    build: init push

---
+++ app/ui/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+// Specify the font and its subsets
+export const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
